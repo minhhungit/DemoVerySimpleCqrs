a304563a897be546589f473f9cd3207b20bf13a3
Jin <it.minhhung@gmail.com>
2020-04-25 09:13:48 +0700

    add note

--- DemoVerySimpleCqrs/Program.cs
+++ DemoVerySimpleCqrs/Program.cs
@@ -12,14 +12,20 @@ namespace DemoVerySimpleCqrs
             var b = new Bootstrapper();
             b.Start();
 
+            // WRITE SIDE
             var supplierName = "Jin";
             var createSupplierCmd = new CreateSupplierCmd(newSupplierId, supplierName);
-
             Bootstrapper.CommandBus.ProcessCommand(createSupplierCmd);
 
             // add 2 new locations
             Bootstrapper.CommandBus.ProcessCommand(new AddSupplierLocationCmd(newSupplierId, Guid.NewGuid(), "AAA"));
             Bootstrapper.CommandBus.ProcessCommand(new AddSupplierLocationCmd(newSupplierId, Guid.NewGuid(), "BBB"));
+
+            // ======================================================
+
+            // READ SIDE: get data from [views]
+            // create sql query to get data from 2 view tables: FirstView & SecondView
+            // ex: dapper.Query<SecondViewModel>(x=>x.LocationName = "BBB");
 
             Console.ReadKey();
         }
